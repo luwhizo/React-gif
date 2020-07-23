6b3bc60c498b9ef9ff763fea8eb888f54ed25591
Luis Mamani Surco <luwhizo@gmail.com>
2020-07-23 17:52:29 -0400

    docs agregado

---
+++ docs/precache-manifest.f4c7a77d85ad1b94f8fbe753422292c4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a0c7aa3f9894ea3c1e6a3a43e8e1aaf5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e6e8fcb5c5aaa5b69e19",
+    "url": "/static/css/main.d4763670.chunk.css"
+  },
+  {
+    "revision": "a50d5cb98697f7283138",
+    "url": "/static/js/2.0ba6f1d5.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.0ba6f1d5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e6e8fcb5c5aaa5b69e19",
+    "url": "/static/js/main.9bb9c414.chunk.js"
+  },
+  {
+    "revision": "2beaf3c9299542d4240e",
+    "url": "/static/js/runtime-main.c8ba4116.js"
+  }
+]);
